10c19664f8617ef4d620d8168253cca0652c784e
brianbender <brian@enlearn.org>
2016-07-19 15:42:12 -0700

    doin a blackjack

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -7,7 +7,7 @@ namespace Blackjack
         public static void Main(string[] args)
         {
             var money = 500;
-            Console.WriteLine("Welcome to blackjack. You have $500. Each hand costs $25. You win at $1000 or more.");
+            Console.WriteLine("Welcome to blackjack. You have $500. Each hand costs $25. You win at $1000.");
 
             while (money > 0)
             {
@@ -24,7 +24,7 @@ namespace Blackjack
                     Console.WriteLine("Do you (h)it or (s)tay?");
                     input = Console.ReadKey().KeyChar.ToString();
                 }
-
+                Console.WriteLine();
                 if (input == "s")
                 {
                     Console.WriteLine(Environment.NewLine +
@@ -38,9 +38,8 @@ namespace Blackjack
                     Console.WriteLine($"The dealer slides another card to you. It's a {GetCardName(newCard)}.");
                 }
 
-
-                var yourCards = yourHand.Item1 + yourHand.Item2 + newCard;
-                if (yourCards < dealerHand.Item1 + dealerHand.Item2 || yourCards > 21)
+                var yourCards = GetCardValue(yourHand.Item1) + GetCardValue(yourHand.Item2) + GetCardValue(newCard);
+                if (yourCards < GetCardValue(dealerHand.Item1) + GetCardValue(dealerHand.Item2) || yourCards > 21)
                 {
                     money -= 25;
                     Console.WriteLine($"You did a blackjack and lost. You now have ${money} (-$25)");
@@ -61,6 +60,13 @@ namespace Blackjack
 
             Console.WriteLine("You lose.");
             Console.ReadKey();
+        }
+
+        private static int GetCardValue(int card)
+        {
+            if (card > 10)
+                return 10;
+            return card;
         }
 
         private static string GetCardName(int card)
