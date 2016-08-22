22404f3d726cd828dbfcb3ab2c3bbeaec30ad5ea
Brian Bender <brian@enlearn.org>
2016-08-22 09:34:25 -0700

    Updated coverage script

--- runcoverage.bat
+++ runcoverage.bat
@@ -1,3 +1,5 @@
-Dependencies\OpenCover.4.6.519\tools\OpenCover.Console.exe "-target:dependencies\nunit-console\nunit3-console.exe" -targetargs:"BlackjackTests\bin\Debug\BlackjackTests.dll --noheader --shadowcopy" "-searchdirs:Blackjack\bin\Debug\" -register:user
+call "%VS140COMNTOOLS%"\\vsvars32.bat
+msbuild Blackjack.sln /p:Configuration=Debug /m /nologo /verbosity:quiet
+Dependencies\OpenCover.4.6.519\tools\OpenCover.Console.exe "-target:dependencies\nunit-console\nunit3-console.exe" "-targetargs:BlackjackTests\bin\Debug\BlackjackTests.dll" "-searchdirs:Blackjack\bin\Debug\" -register:path32 -showunvisited
 Dependencies\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe "-reports:results.xml" "-targetdir:report"
 start chrome report/index.htm
